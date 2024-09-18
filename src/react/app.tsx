554ef9ecc8cbcf4c6a618dcc991dfe2866df26ba
markshawn2020 <shawninjuly@gmail.com>
2024-09-19 07:01:24 +0800

    [exts_chrome_chatbot-exporter] added themr

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,14 +1,1 @@
-import { Trigger } from "./trigger";
-import React from "react";
-import ReactDOM from "react-dom";
-
 import "../../../../assets/styles/main.css";
-
-export const mountReactApp = (container: HTMLElement) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Trigger />
-    </React.StrictMode>,
-    container,
-  );
-};
