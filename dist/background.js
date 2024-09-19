9b2dada54afc0a5d27f8ecf00aa34703c0528cff
markshawn2020 <shawninjuly@gmail.com>
2024-09-20 05:54:15 +0800

    fixed renaming, webpack split problem

--- dist/background.js
+++ dist/background.js
@@ -1,8 +1,33 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
+"use strict";
+(self["webpackChunk_cs_magic_exts_chrome_claude_artifact_enhancer"] = self["webpackChunk_cs_magic_exts_chrome_claude_artifact_enhancer"] || []).push([["background"],{
 
-/***/ "./src/utils/const.ts":
+/***/ 351:
+/*!********************************!*\
+  !*** ./src/core/background.ts ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _utils_const__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../utils/const */ 498);
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // 这个已经不需要了，因为直接在前端就可以拿到
+    if (request.action === _utils_const__WEBPACK_IMPORTED_MODULE_0__.action.fetchConversationChats) {
+        console.log("-- fetchConversationChats");
+        fetch(request.url, {
+            credentials: "include",
+        })
+            .then((response) => response.json())
+            .then((data) => sendResponse({ data: data }))
+            .catch((error) => sendResponse({ error: error.toString() }));
+        return true; // 保持消息通道开放
+    }
+});
+
+
+/***/ }),
+
+/***/ 498:
 /*!****************************!*\
   !*** ./src/utils/const.ts ***!
   \****************************/
@@ -50,83 +75,10 @@ const lucideArrowBigDownDashSvgString = `<svg xmlns="http://www.w3.org/2000/svg"
 
 /***/ })
 
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-/*!********************************!*\
-  !*** ./src/core/background.ts ***!
-  \********************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _utils_const__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../utils/const */ "./src/utils/const.ts");
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    // 这个已经不需要了，因为直接在前端就可以拿到
-    if (request.action === _utils_const__WEBPACK_IMPORTED_MODULE_0__.action.fetchConversationChats) {
-        console.log("-- fetchConversationChats");
-        fetch(request.url, {
-            credentials: "include",
-        })
-            .then((response) => response.json())
-            .then((data) => sendResponse({ data: data }))
-            .catch((error) => sendResponse({ error: error.toString() }));
-        return true; // 保持消息通道开放
-    }
-});
-
-/******/ })()
-;
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(351));
+/******/ }
+]);
 //# sourceMappingURL=background.js.map
