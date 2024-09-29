470e14d89720df851e70713219da92c60d9bf249
markshawn2020 <shawninjuly@gmail.com>
2024-09-29 19:32:02 +0800

    fixed build

--- src/utils/doCopyPng.ts
+++ src/utils/doCopyPng.ts
@@ -1,11 +1,15 @@
-import { svgString2pngBlob } from "@cs-magic/common-frontend/svgString2pngBlob";
 import { blob2clipboard } from "@cs-magic/common-frontend/blob2clipboard";
+import {
+  svgBlob2pngBlob,
+  svgString2svgBlob,
+} from "@cs-magic/common-frontend/svgString2pngBlob";
 
 export const doCopyPng = async () => {
   document.querySelector(".fixed .sticky h3").textContent;
   const svgString = window.svgString;
   if (!svgString) return alert("no svgString now");
 
-  const pngBlog = await svgString2pngBlob(svgString);
+  const svgBlob = svgString2svgBlob(svgString);
+  const pngBlog = await svgBlob2pngBlob(svgBlob);
   void blob2clipboard(pngBlog);
 };
